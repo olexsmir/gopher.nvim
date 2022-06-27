ebb3894b3bcce06768f3cf7052e00d163a57f239
Smirnov Oleksandr <ss2316544@gmail.com>
2022-06-27 14:30:02 +0300

    fix(_utils): set notification level

--- lua/gopher/_utils/init.lua
+++ lua/gopher/_utils/init.lua
@@ -38,10 +38,19 @@ return {
   end,
 
   ---@param msg string
-  ---@param lvl string
+  ---@param lvl string|integer
   notify = function(msg, lvl)
+    local l
+    if lvl == "error" or lvl == 4 then
+      l = vim.log.levels.ERROR
+    elseif lvl == "info" or lvl == 2 then
+      l = vim.log.levels.INFO
+    elseif lvl == "debug" or lvl == 1 then
+      l = vim.log.levels.DEBUG
+    end
+
     vim.defer_fn(function()
-      vim.notify(msg, lvl)
+      vim.notify(msg, l)
     end, 0)
   end,
 }
