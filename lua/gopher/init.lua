bab71dd6b2081eef168fc85951db97c9edfba3ba
Smirnov Oleksandr <ss2316544@gmail.com>
2022-06-29 23:42:07 +0300

    feat(comments): first naive implementation

--- lua/gopher/init.lua
+++ lua/gopher/init.lua
@@ -9,6 +9,7 @@ gopher.mod = require "gopher.gomod"
 gopher.get = require "gopher.goget"
 gopher.impl = require "gopher.impl"
 gopher.generate = require "gopher.gogenerate"
+gopher.comment = require "gopher.comment"
 gopher.test_add = gotests.func_test
 gopher.test_exported = gotests.all_exported_tests
 gopher.tests_all = gotests.all_tests
