9f6e94f46f47158f4f46d988a072111a05f8452a
Smirnov Oleksandr <ss2316544@gmail.com>
2022-09-22 17:34:30 +0300

    feat(health): add check for nvim-dap

--- lua/gopher/health.lua
+++ lua/gopher/health.lua
@@ -2,11 +2,13 @@ local health = vim.health or require "health"
 local utils = require "gopher._utils"
 local c = require("gopher.config").config.commands
 
+local requried = "Gopher.nvim will not work without it!"
 local M = {
   _required = {
     plugins = {
-      { lib = "plenary" },
-      { lib = "nvim-treesitter" },
+      { lib = "plenary", help = requried },
+      { lib = "nvim-treesitter", help = requried },
+      { lib = "dap", help = "Required for set upping debugger" },
     },
     binarys = {
       { bin = c.go, help = "required for GoMod, GoGet, GoGenerate command" },
@@ -23,7 +25,7 @@ function M.check()
     if utils.lualib_is_found(plugin.lib) then
       health.report_ok(plugin.lib .. " installed.")
     else
-      health.report_error(plugin.lib .. " not found. Gopher.nvim will not work without it!")
+      health.report_error(plugin.lib .. " not found. " .. plugin.help)
     end
   end
 
