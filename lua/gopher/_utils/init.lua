0b4e0b7449df34c8baf7c15b20836dc4a2393f78
Smirnov Oleksandr <ss2316544@gmail.com>
2022-10-07 17:12:49 +0300

    refactor(utils): separete module for health

--- lua/gopher/_utils/init.lua
+++ lua/gopher/_utils/init.lua
@@ -20,23 +20,6 @@ return {
     return s:sub(1, n)
   end,
 
-  ---@param lib string
-  ---@return boolean
-  lualib_is_found = function(lib)
-    local is_found, _ = pcall(require, lib)
-    return is_found
-  end,
-
-  ---@param bin string
-  ---@return boolean
-  binary_is_found = function(bin)
-    if vim.fn.executable(bin) == 1 then
-      return true
-    end
-
-    return false
-  end,
-
   ---@param msg string
   ---@param lvl string|integer
   notify = function(msg, lvl)
