eb927bc19b7fc20ef0a1bde02122d96101bdccbf
Smirnov Oleksandr <ss2316544@gmail.com>
2022-11-13 14:19:36 +0200

    refactor(dap): move import into function

--- lua/gopher/dap/init.lua
+++ lua/gopher/dap/init.lua
@@ -1,9 +1,10 @@
-local cfg = require "gopher.dap.config"
-local u = require "gopher._utils"
 local M = {}
 
 ---setup nvim-dap for golang using
 function M.setup()
+  local cfg = require "gopher.dap.config"
+  local u = require "gopher._utils"
+
   local dap = u.sreq "dap"
   dap.adapters.go = cfg.adapter
   dap.configurations.go = cfg.configuration
