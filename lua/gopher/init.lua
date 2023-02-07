59de7dc72d1fa333c1d86eb618d1e9adfea24977
Smirnov Oleksandr <ss2316544@gmail.com>
2023-02-07 15:57:28 +0200

    remove api module

--- lua/gopher/init.lua
+++ lua/gopher/init.lua
@@ -1,5 +1,29 @@
-local GOPHER = {}
+local stags = require "gopher.struct_tags"
+local tests = require "gopher.gotests"
+local cmd = require "gopher._utils.commands"
+local gopher = {}
 
-GOPHER.setup = require("gopher.config").setup
+gopher.setup = require("gopher.config").setup
+gopher.install_deps = require "gopher.installer"
+gopher.tags_add = stags.add
+gopher.tags_rm = stags.remove
+gopher.impl = require "gopher.impl"
+gopher.iferr = require "gopher.iferr"
+gopher.comment = require "gopher.comment"
+gopher.test_add = tests.func_test
+gopher.test_exported = tests.all_exported_tests
+gopher.tests_all = tests.all_tests
+gopher.get = function(...)
+  cmd("get", ...)
+end
+gopher.mod = function(...)
+  cmd("mod", ...)
+end
+gopher.generate = function(...)
+  cmd("generate", ...)
+end
+gopher.work = function(...)
+  cmd("work", ...)
+end
 
-return GOPHER
+return gopher
