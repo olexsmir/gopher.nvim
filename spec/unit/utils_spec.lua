82e739d823e9dbe834203531cd794288c5a724c4
Smirnov Oleksandr <ss2316544@gmail.com>
2023-07-05 13:55:28 +0300

    test(_utils): test .sreq method

--- spec/unit/utils_spec.lua
+++ spec/unit/utils_spec.lua
@@ -12,4 +12,18 @@ describe("gopher._utils", function()
       assert.are.same(res, true)
     end)
   end)
+
+  describe(".sreq()", function()
+    local sreq = require("gopher._utils").sreq
+
+    it("return module if it exists", function()
+      assert.are.same(sreq "gopher", require "gopher")
+    end)
+
+    it("throw error if it doesn't exist", function()
+      assert.has.errors(function()
+        sreq "no_existing_module"
+      end)
+    end)
+  end)
 end)
