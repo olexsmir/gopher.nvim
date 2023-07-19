1c0eeb3a861ede380555b1f714d45980cec8117d
Smirnov Oleksandr <ss2316544@gmail.com>
2023-07-19 19:56:34 +0300

    refactor: move all plugin functionality to init.lua

--- lua/gopher/init.lua
+++ lua/gopher/init.lua
@@ -1,5 +1,35 @@
-local GOPHER = {}
+local tags = require "gopher.struct_tags"
+local tests = require "gopher.gotests"
+local uc = require "gopher._utils.commands"
+local gopher = {}
 
-GOPHER.setup = require("gopher.config").setup
+gopher.setup = require("gopher.config").setup
+gopher.install_deps = require "gopher.installer"
+gopher.impl = require "gopher.impl"
+gopher.iferr = require "gopher.iferr"
+gopher.comment = require "gopher.comment"
 
-return GOPHER
+gopher.tags_add = tags.add
+gopher.tags_rm = tags.remove
+
+gopher.test_add = tests.func_test
+gopher.test_exported = tests.all_exported_tests
+gopher.tests_all = tests.all_tests
+
+gopher.get = function(...)
+  uc("get", ...)
+end
+
+gopher.mod = function(...)
+  uc("mod", ...)
+end
+
+gopher.generate = function(...)
+  uc("generate", ...)
+end
+
+gopher.work = function(...)
+  uc("work", ...)
+end
+
+return gopher
