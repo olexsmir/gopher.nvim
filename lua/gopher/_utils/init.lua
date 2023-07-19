616474b919f97296d71d2ee9d05914dda55d9f10
Smirnov Oleksandr <ss2316544@gmail.com>
2023-07-19 23:20:18 +0300

    refactor(_utils): change way how it organizes

--- lua/gopher/_utils/init.lua
+++ lua/gopher/_utils/init.lua
@@ -1,48 +1,39 @@
----@diagnostic disable: param-type-mismatch
-return {
-  ---@param t table
-  ---@return boolean
-  empty = function(t)
-    if t == nil then
-      return true
-    end
+local utils = {}
 
-    return next(t) == nil
-  end,
+---@param t table
+---@return boolean
+function utils.is_tbl_empty(t)
+  if t == nil then
+    return true
+  end
+  return next(t) == nil
+end
 
-  ---@param s string
-  ---@return string
-  rtrim = function(s)
-    local n = #s
-    while n > 0 and s:find("^%s", n) do
-      n = n - 1
-    end
+---@param s string
+---@return string
+function utils.rtrim(s)
+  local n = #s
+  while n > 0 and s:find("^%s", n) do
+    n = n - 1
+  end
 
-    return s:sub(1, n)
-  end,
+  return s:sub(1, n)
+end
 
-  ---@param msg string
-  ---@param lvl string|integer
-  notify = function(msg, lvl)
-    local l
-    if lvl == "error" or lvl == 4 then
-      l = vim.log.levels.ERROR
-    elseif lvl == "info" or lvl == 2 then
-      l = vim.log.levels.INFO
-    elseif lvl == "debug" or lvl == 1 then
-      l = vim.log.levels.DEBUG
-    end
+---@param msg string
+---@param lvl any
+function utils.deferred_notify(msg, lvl)
+  vim.defer_fn(function()
+    vim.notify(msg, lvl)
+  end, 0)
+end
 
-    vim.defer_fn(function()
-      vim.notify(msg, l)
-    end, 0)
-  end,
+-- safe require
+---@param  module string module name
+function utils.sreq(module)
+  local ok, m = pcall(require, module)
+  assert(ok, string.format("gopher.nvim dependency error: %s not installed", module))
+  return m
+end
 
-  ---safe require
-  ---@param name string module name
-  sreq = function(name)
-    local ok, m = pcall(require, name)
-    assert(ok, string.format("gopher.nvim dependency error: %s not installed", name))
-    return m
-  end,
-}
+return utils
