e2ef9dbcd707fbeb46f2149ce8a10eea1cf82341
Smirnov Oleksandr <ss2316544@gmail.com>
2023-08-10 11:47:24 +0300

    refactor(utils): notify now has title

--- lua/gopher/_utils/init.lua
+++ lua/gopher/_utils/init.lua
@@ -1,5 +1,7 @@
 local utils = {}
 
+local TITLE = "gopher.nvim"
+
 ---@param t table
 ---@return boolean
 function utils.is_tbl_empty(t)
@@ -10,11 +12,22 @@ function utils.is_tbl_empty(t)
 end
 
 ---@param msg string
----@param lvl any
+---@param lvl number
 function utils.deferred_notify(msg, lvl)
   vim.defer_fn(function()
-    vim.notify(msg, lvl)
+    vim.notify(msg, lvl, {
+      title = TITLE,
+    })
   end, 0)
+end
+
+---@param msg string
+---@param lvl? number
+function utils.notify(msg, lvl)
+  lvl = lvl or vim.log.levels.INFO
+  vim.notify(msg, lvl, {
+    title = TITLE,
+  })
 end
 
 -- safe require
