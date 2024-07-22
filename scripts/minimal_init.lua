07a4a710f4a39fc771a8f411587528d9b304f6bf
Smirnov Oleksandr <ss2316544@gmail.com>
2024-07-23 00:24:13 +0300

    feat: setup mini.test

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -26,9 +26,13 @@ vim.notify = print
 
 install_plug "nvim-lua/plenary.nvim"
 install_plug "nvim-treesitter/nvim-treesitter"
+install_plug "echasnovski/mini.test" -- used for tests
 install_plug "echasnovski/mini.doc" -- used for docs generation
 
 vim.env.XDG_CONFIG_HOME = root ".tests/config"
 vim.env.XDG_DATA_HOME = root ".tests/data"
 vim.env.XDG_STATE_HOME = root ".tests/state"
 vim.env.XDG_CACHE_HOME = root ".tests/cache"
+
+-- setup
+require("mini.test").setup()
