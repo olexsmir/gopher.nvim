afae6dbb04f84f7a7f585f55a29e1fccb7de6a7f
Oleksandr Smirnov <olexsmir@gmail.com>
2025-02-17 19:12:42 +0200

    test: add boilerpates

---
+++ spec/integration/impl_test.lua
@@ -0,0 +1,14 @@
+local t = require "spec.testutils"
+
+local child = MiniTest.new_child_neovim()
+local T = MiniTest.new_set {
+  hooks = {
+    post_once = child.stop,
+    pre_case = function()
+      child.restart { "-u", "scripts/minimal_init.lua" }
+    end,
+  },
+}
+T["impl"] = MiniTest.new_set {}
+
+return T
