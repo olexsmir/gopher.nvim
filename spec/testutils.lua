4b0fd060f9638669ff5176d697a5e5794e4fdae2
Oleksandr Smirnov <olexsmir@gmail.com>
2025-02-24 13:22:41 +0200

    refactor(testutils): remove unnecessary code

--- spec/testutils.lua
+++ spec/testutils.lua
@@ -31,18 +31,12 @@ function testutils.writefile(fpath, contents)
   vim.fn.writefile(vim.split(contents, "\n"), fpath)
 end
 
----@param path string
----@return string
-function testutils.readfile_from_fixture_dir(path)
-  return testutils.readfile(fixtures_dir .. path)
-end
-
 ---@param fixture string
 ---@return {input: string, output: string}
 function testutils.get_fixtures(fixture)
   return {
-    input = testutils.readfile_from_fixture_dir(fixture .. "_input.go"),
-    output = testutils.readfile_from_fixture_dir(fixture .. "_output.go"),
+    input = testutils.readfile(fixtures_dir .. fixture .. "_input.go"),
+    output = testutils.readfile(fixtures_dir .. fixture .. "_output.go"),
   }
 end
 
