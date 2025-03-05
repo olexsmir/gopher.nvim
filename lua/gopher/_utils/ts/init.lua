fec8a74fe3853a554010da9a54e606e60d9561c0
Oleksandr Smirnov <olexsmir@gmail.com>
2025-03-05 16:22:00 +0200

    fix: typos

--- lua/gopher/_utils/ts/init.lua
+++ lua/gopher/_utils/ts/init.lua
@@ -2,7 +2,7 @@
 local nodes = require "gopher._utils.ts.nodes"
 local u = require "gopher._utils"
 local ts = {
-  querys = {
+  queries = {
     struct_block = [[((type_declaration (type_spec name:(type_identifier) @struct.name type: (struct_type)))@struct.declaration)]],
     em_struct_block = [[(field_declaration name:(field_identifier)@struct.name type: (struct_type)) @struct.declaration]],
     package = [[(package_clause (package_identifier)@package.name)@package.clause]],
@@ -25,10 +25,9 @@ end
 ---@param row string
 ---@param col string
 ---@param bufnr string|nil
----@param do_notify boolean|nil
 ---@return table|nil
 function ts.get_struct_node_at_pos(row, col, bufnr)
-  local query = ts.querys.struct_block .. " " .. ts.querys.em_struct_block
+  local query = ts.queries.struct_block .. " " .. ts.queries.em_struct_block
   local bufn = bufnr or vim.api.nvim_get_current_buf()
   local ns = nodes.nodes_at_cursor(query, get_name_defaults(), bufn, row, col)
   if ns == nil then
@@ -41,10 +40,9 @@ end
 ---@param row string
 ---@param col string
 ---@param bufnr string|nil
----@param do_notify boolean|nil
 ---@return table|nil
 function ts.get_func_method_node_at_pos(row, col, bufnr)
-  local query = ts.querys.func .. " " .. ts.querys.method_name
+  local query = ts.queries.func .. " " .. ts.queries.method_name
   local bufn = bufnr or vim.api.nvim_get_current_buf()
   local ns = nodes.nodes_at_cursor(query, get_name_defaults(), bufn, row, col)
   if ns == nil then
@@ -57,13 +55,12 @@ end
 ---@param row string
 ---@param col string
 ---@param bufnr string|nil
----@param do_notify boolean|nil
 ---@return table|nil
 function ts.get_package_node_at_pos(row, col, bufnr)
   if row > 10 then
     return
   end
-  local query = ts.querys.package
+  local query = ts.queries.package
   local bufn = bufnr or vim.api.nvim_get_current_buf()
   local ns = nodes.nodes_at_cursor(query, get_name_defaults(), bufn, row, col)
   if ns == nil then
@@ -76,10 +73,9 @@ end
 ---@param row string
 ---@param col string
 ---@param bufnr string|nil
----@param do_notify boolean|nil
 ---@return table|nil
 function ts.get_interface_node_at_pos(row, col, bufnr)
-  local query = ts.querys.interface
+  local query = ts.queries.interface
   local bufn = bufnr or vim.api.nvim_get_current_buf()
   local ns = nodes.nodes_at_cursor(query, get_name_defaults(), bufn, row, col)
   if ns == nil then
