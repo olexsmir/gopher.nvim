03de1f5ffcf09ae5ac4eca2fa989c8b490adfca4
Oleksandr Smirnov <olexsmir@gmail.com>
2025-10-30 16:28:02 +0200

    chore: fix formatting

--- spec/integration/iferr_test.lua
+++ spec/integration/iferr_test.lua
@@ -14,7 +14,8 @@ iferr["should add if err with custom message"] = function()
   child.lua [[
     require("gopher").setup {
       iferr = { message = 'fmt.Errorf("failed to %w", err)' }
-  } ]]
+    }
+  ]]
 
   local rs = t.setup_test("iferr/message", child, { 6, 2 })
   child.cmd "GoIfErr"
